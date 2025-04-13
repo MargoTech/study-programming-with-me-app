95c1dc6c377bbbb1e0eb6b4f7204e125815f1d73
MargoTech <margobartashevich@gmail.com>
2025-04-13 23:37:05 +0100

    Adding Quiz.jsx

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,6 +1,25 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const Quiz = () => {
-  const { id } = useParams();
-  console.log(id);
+    const { id } = useParams();
+    console.log(id);
+  };  
+
+const [questions, setQuestions] = useState([]);
+const [loading, setLoading] = useState(true);
+
+return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Тест по теме: {id}</h1>
+
+      {loading ? (
+        <p>Загрузка...</p>
+      ) : (
+        <pre>{JSON.stringify(questions, null, 2)}</pre>
+      )}
+    </div>
+  );
 };
+
+export default Quiz;
