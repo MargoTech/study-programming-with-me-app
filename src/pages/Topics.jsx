dd9c225a907a3f0f9b22f2fb8bcc94715134b596
MargoTech <margobartashevich@gmail.com>
2025-04-17 23:56:39 +0100

    Working with styles scc

--- src/pages/Topics.jsx
+++ src/pages/Topics.jsx
@@ -4,10 +4,16 @@ const topics = [
   {
     id: "html",
     title: "HTML for beginners",
-    image: "",
+    image: "https://cdn-icons-png.flaticon.com/512/732/732212.png",
   },
-  { id: "css", title: "Basic CSS" },
-  { id: "js", title: "Basic JavaScript" },
+  { id: "css", 
+    title: "Basic CSS", 
+    image:"https://cdn-icons-png.flaticon.com/512/732/732190.png",
+    {
+      id: "js",
+      title: "Basic JavaScript",
+      image: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png", // JS icon
+    },
 ];
 
 const Topics = () => {
@@ -18,15 +24,15 @@ const Topics = () => {
       {topics.map((topic) => (
         <div
           key={topic.id}
-          className="bg-white p-6 rounded-xl shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out transform hover:scale-105 cursor-pointer hover:bg-gradient-to-r hover:from-indigo-500 hover:to-purple-500"
+          className="bg-white p-6 rounded-xl shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out transform hover:scale-105 cursor-pointer hover:bg-gradient-to-r hover:from-teal-400 hover:to-pink-300"
           onClick={() => navigate(`/learn/${topic.id}`)}
         >
           <img
             src={topic.image}
             alt={topic.title}
-            className="w-full h-40 object-cover rounded-t-xl mb-4"
+            className="w-20 h-20 object-contain mx-auto mb-4"
           />
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+          <h2 className="text-xl font-semibold text-center text-gray-800">
             {topic.title}
           </h2>
           <p className="text-gray-600 text-sm">Click to start learning</p>
